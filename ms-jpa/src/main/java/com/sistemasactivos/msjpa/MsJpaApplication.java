2f36462241057cf120de453c018e2d1be67a91fd
tgriffabenitez <tgriffabenitez@gmail.com>
2023-04-27 10:36:33 -0300

    inicio del proyecto

---
+++ ms-jpa/src/main/java/com/sistemasactivos/msjpa/MsJpaApplication.java
@@ -0,0 +1,13 @@
+package com.sistemasactivos.msjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsJpaApplication.class, args);
+    }
+
+}
---
+++ ms-jpa/src/test/java/com/sistemasactivos/msjpa/MsJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemasactivos.msjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
