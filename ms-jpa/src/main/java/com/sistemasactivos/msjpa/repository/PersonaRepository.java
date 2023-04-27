e4828d520d5017e503dee028cf7dbc4bb4973ec1
tgriffabenitez <tgriffabenitez@gmail.com>
2023-04-27 14:43:33 -0300

    Elimine los repositorios ya que no tengo que hacer la capa de servicios ni la de controlador

--- ms-jpa/src/main/java/com/sistemasactivos/msjpa/repository/PersonaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sistemasactivos.msjpa.repository;
-
-import com.sistemasactivos.msjpa.model.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
-}
--- ms-jpa/src/main/java/com/sistemasactivos/msjpa/repository/RolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sistemasactivos.msjpa.repository;
-
-import com.sistemasactivos.msjpa.model.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RolRepository extends JpaRepository<Rol, Long> {
-}
